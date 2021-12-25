5ffcfc25fbd5f3d72a84d6b11b36858b2b0da8e6
Dean <dean@deansantillan.com>
2021-12-25 13:58:43 +1000

    task(): created functions to retrieve api photos and too display them.

--- app.js
+++ app.js
@@ -1,1 +1,58 @@
+// DOM
+const imagesContainer = document.getElementById('image-container');
+const loadingSpinner = document.getElementById('loader');
+let unsplashPhotosArray = [];
+const imageCardHtml = ``;
 
+// API Constants
+const unsplashApiKey = '_xX9QNKSa09FknegMCxL2Z1GPZ5EdAQCFWrpmzx53lQ';
+const unsplashImageResults = 10;
+const unsplashApiUrl = `https://api.unsplash.com/photos/random/?client_id=${unsplashApiKey}&count=${unsplashImageResults}`;
+
+//
+// const uiTemplate = `
+//     <div class="card" id="card">
+//         <a href=${} title=${}><img src=${} alt=${}></a>
+//     </div>`;
+//
+const getUnsplashApiPhotos = async () => {
+	try {
+		const response = await fetch(unsplashApiUrl);
+		unsplashPhotosArray = await response.json();
+		displayPhotos();
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+//
+const displayPhotos = () => {
+	unsplashPhotosArray.forEach((photo) => {
+		renderPhotoElements(
+			photo.links.html,
+			photo.urls.regular,
+			photo.alt_description
+		);
+	});
+};
+const setElementAttributes = (element, attributes) => {
+	for (const key in attributes) {
+		element.setAttribute(key, attributes[key]);
+	}
+};
+const renderPhotoElements = (linkUrl, photoUrl, altDescription) => {
+	const linkElement = document.createElement('a');
+	setElementAttributes(linkElement, { href: linkUrl, target: '_blank' });
+
+	const imgElement = document.createElement('img');
+	setElementAttributes(imgElement, {
+		src: photoUrl,
+		alt: altDescription,
+		title: altDescription,
+	});
+
+	linkElement.appendChild(imgElement);
+	imagesContainer.appendChild(linkElement);
+};
+
+getUnsplashApiPhotos();
